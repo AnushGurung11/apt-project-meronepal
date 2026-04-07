fa1549a96f35726256c16a1c2735ee97e88794fa
AnushGurung11 <anushgrg15@gmail.com>
2026-04-07 22:18:23 +0545

    Creating Project Folder

---
+++ src/main/java/com/aptproject/meronepal/contoller/HomeServlet.java
@@ -0,0 +1,16 @@
+package com.aptproject.meronepal.contoller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request,response);
+    }
+}

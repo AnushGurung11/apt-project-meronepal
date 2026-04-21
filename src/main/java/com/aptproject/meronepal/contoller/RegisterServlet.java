d9521040e3cfb8e1b4e2ebb67f2606fe7a750f32
AnushGurung11 <anushgrg15@gmail.com>
2026-04-21 09:14:43 +0545

    example

--- src/main/java/com/aptproject/meronepal/contoller/RegisterServlet.java
+++ src/main/java/com/aptproject/meronepal/contoller/RegisterServlet.java
@@ -18,87 +18,17 @@ import java.io.IOException;
 // Validating Phone number field for string
 
 // Servlet which handles registration
-@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
+@WebServlet(name = "RegisterServlet", urlPatterns = { "/register" })
 public class RegisterServlet extends HttpServlet {
 
     // DAO object created to interact with database
-    //Database bata data ko lagi banauna parne DAO
+    // Database bata data ko lagi banauna parne DAO
     private final UserDAO userDAO = new UserDAO();
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-//        rd.forward(request, response);
-        response.getWriter().println("Register page - GET working!");
-    }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        // Get data from the registration form
-        String userName = request.getParameter("username");
-        String phoneNumber = request.getParameter("phoneNumber");
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String confirmPassword = request.getParameter("confirmPassword");
-
-
-        final boolean isValidName = !ValidationUtil.isNullOrEmpty(userName)
-                && ValidationUtil.isAlphanumericStartingWithLetter(userName)
-                && userName.length() > 5;
-        String errorUser = isValidName ? "" : "Name not Proper! ";
-
-        final boolean isValidMail = ValidationUtil.isValidEmail(email);
-        String errorMail = isValidMail ? "" : "Mail not Proper! ";
-
-        final boolean isValidNum = ValidationUtil.isValidPhone(phoneNumber);
-        String errorNum = isValidNum ? "" : "Not a Proper Phone number !";
-
-        final boolean isValidPass = ValidationUtil.isValidPassword(password);
-        String errorPass = isValidPass ? "" : "Password not Proper! ";
-
-        final boolean isValidCon = ValidationUtil.doPasswordsMatch(password, confirmPassword);
-        String errorCon = isValidCon ? "" : "Password not matching! ";
-
-        String error_ = errorUser + errorMail + errorPass + errorCon;
-        request.setAttribute("error", error_);
-        request.setAttribute("erUser", errorUser);
-        request.setAttribute("erMail", errorMail);
-
-        //if error in user data provide feedback to same page
-        if (!error_.isBlank()) {
-            response.getWriter().println("Validation Error: " + error_);
-
-//            RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
-//            rd.forward(request, response);
-        }else {
-            // if user details all good, hash it before storing it
-            String hashedPassword = PasswordUtil.getHashPassword(password);
-
-            //Create user entry on database users table
-            final UserDAO userDAO = new UserDAO();
-            int check = userDAO.insertUser(userName, email,  phoneNumber, hashedPassword);
-            switch (check) {
-                case 1:
-                    //After successful login we will save the user to the session and redirect to the Home page
-                    //TODO here is the redirection
-//                    response.sendRedirect("Login.jsp");
-                    response.getWriter().println("Registration Successful!");
-                    break;
-                //if check is 2 user already present display user already present error
-                //send user present message to user in register page
-                case 2:
-//                    request.setAttribute("error", "User/Email already present!");
-                    response.getWriter().println("Error: User/Email already present!");
-//                    RequestDispatcher rdisp = request.getRequestDispatcher("/register.jsp");
-//                    rdisp.forward(request, response);
-                    break;
-                default:
-                    response.getWriter().println("Server Error: " + check);
-                    System.out.println("Server error: " + check + " :error code");
-                    break;
-            }
-        }
-
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO get the parameter from the front end
+        // TODO variable ma store gara
     }
+
 }

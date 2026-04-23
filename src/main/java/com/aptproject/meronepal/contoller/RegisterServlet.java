790a9c1e9f342e348a60775c098678e2d7072564
AnushGurung11 <anushgrg15@gmail.com>
2026-04-23 10:38:48 +0545

    Created filter for authentication

--- src/main/java/com/aptproject/meronepal/contoller/RegisterServlet.java
+++ src/main/java/com/aptproject/meronepal/contoller/RegisterServlet.java
@@ -24,7 +24,11 @@ import java.io.IOException;
 public class RegisterServlet extends HttpServlet {
 
     private final UserDAO userDAO = new UserDAO();
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+        rd.forward(request, response);
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
